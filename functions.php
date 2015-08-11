02c0d541106ccaedf6a3f2974bd464254be15b3c
oakwoodgates <funkyoaktree@gmail.com>
2015-08-11 09:46:13 -0400

    bootstrapalappin

--- functions.php
+++ functions.php
@@ -113,15 +113,22 @@ add_action( 'widgets_init', 'happyhour_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
+// Playfair+Display:400,400italic,700,700italic,900,900italic
+// Lato:400,100,300,300italic,400italic,700,700italic,900,900italic,100italic
 function happyhour_scripts() {
+	$query_args = array(
+		'family' => 'Playfair+Display:400, 700|Lato:400,100,300,300italic,400italic,700,700italic,900,900italic,100italic',
+		'subset' => 'latin,latin-ext,cyrillic',
+	);
+	wp_enqueue_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
 
 	wp_enqueue_style( 'happyhour-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css' );	
 
-	wp_enqueue_style( 'happyhour-style', get_stylesheet_uri() );
+//	wp_enqueue_style( 'happyhour-style', get_stylesheet_uri() );
 
-//	wp_enqueue_script( 'happyhour-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'happyhour-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-//	wp_enqueue_script( 'happyhour-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'happyhour-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- header.php
+++ header.php
@@ -40,4 +40,4 @@
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+	<div id="content" class="site-content container">
