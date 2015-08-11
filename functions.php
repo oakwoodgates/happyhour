4d2dd2745c62d90d52577b32369771e70729070f
oakwoodgates <funkyoaktree@gmail.com>
2015-08-10 23:42:14 -0400

    sass and bootstrap stuff

--- functions.php
+++ functions.php
@@ -114,15 +114,20 @@ add_action( 'widgets_init', 'happyhour_widgets_init' );
  * Enqueue scripts and styles.
  */
 function happyhour_scripts() {
+
+	wp_enqueue_style( 'happyhour-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css' );	
+
 	wp_enqueue_style( 'happyhour-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'happyhour-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+//	wp_enqueue_script( 'happyhour-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'happyhour-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+//	wp_enqueue_script( 'happyhour-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	wp_enqueue_script( 'happyhour-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '1.0.0', true );	
 }
 add_action( 'wp_enqueue_scripts', 'happyhour_scripts' );
 
