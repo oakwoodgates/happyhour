d9569eb672c887111f6e1f5bc4726bdd0ca274c2
oakwoodgates <funkyoaktree@gmail.com>
2016-08-18 21:22:42 -0400

    pluxh copy changes

--- content-blocks/tulsa/vip_pluxh.php
+++ content-blocks/tulsa/vip_pluxh.php
@@ -15,38 +15,37 @@
 			<div class="col-md-7">
 				<?php echo do_shortcode( '[gallery type="rectangular" size="thumbnail" link="file" ids="2459,2460,2458"]' ); ?>
 				<div class="jumbotron mini-jumbotron bkg-darker">
-	        		<h3>Pluxh</h3>
+					<h3>Pluxh</h3>
 
 					<div class="row marg">
 						<div class="col-xs-12 col-sm-6 panel-club-info">
 							<ul class="list-group">
 								<li class="list-group-item"><i class="fa fa-magic fa-lg fa-fw"></i> Private Stage</li>
 								<li class="list-group-item"><i class="fa fa-key fa-lg fa-fw"></i> Private Bar</li>
-								<li class="list-group-item"><i class="fa fa-users fa-lg fa-fw"></i> Private Security</li>									
+								<li class="list-group-item"><i class="fa fa-users fa-lg fa-fw"></i> Private Security</li>
 							</ul>
 						</div>
 						<div class="col-xs-12 col-sm-6 panel-club-info">
 							<ul class="list-group">
 								<li class="list-group-item"><i class="fa fa-lock fa-lg fa-fw"></i> Private Bathrooms</li>
 								<li class="list-group-item"><i class="fa fa-glass fa-lg fa-fw"></i> Private Drink Menu</li>
-								<li class="list-group-item"><i class="fa fa-thumbs-up fa-lg fa-fw"></i> Non Smoking</li>									
+								<li class="list-group-item"><i class="fa fa-thumbs-up fa-lg fa-fw"></i> Non Smoking</li>
 							</ul>
-						</div>						
+						</div>
 					</div>
 
-	        		<p class="info text-center">For those wanting a more VIP experience on busy nights come experience our private bar &amp; lounge. Open Thursday, Friday &amp; Saturday. $50 entrance fee per person. Come and go all night.</p>
+					<p class="info text-center">For those wanting a more VIP experience on busy nights come experience our private bar &amp; lounge. Open Thursday, Friday &amp; Saturday 7pm to close. <br />Come and go all night.</p>
 
-		        </div>
+				</div>
 			</div>
 <div class="well text-center col-md-10 col-md-offset-1">
 <h4><em>Now Available For Rent</em></h4>
-*Bachelor Party&nbsp;&nbsp;&nbsp;&nbsp;*Divorce Party&nbsp;&nbsp;&nbsp;&nbsp;*Any Party<br /> 
-$300 Booking fee to be paid up front – non-refundable<br />
-<span class="small">*Includes 3 hours of use</span><br />
-Additional charge of $50 for each person in your party. $100 for each additional hour<br />
+*Bachelor Party&nbsp;&nbsp;&nbsp;&nbsp;*Divorce Party&nbsp;&nbsp;&nbsp;&nbsp;*Any Party<br />
+$300 Booking fee for 3 hours of fun for you and up to 20 of your guests. Want more time? Each hour after is only $100.<br />
+<span class="small">Available anytime</span><br />
 Come in and book in person or call us at <a href="tel:9186222858">918.622.3858</a>
-</div>							
-		</div>			
+</div>
+		</div>
 	</div>
 </div>
 
